22383326d595ff779e8863a4866a59a218467285
Idan Peres <90115880+glitch422@users.noreply.github.com>
2024-01-14 11:05:49 -0600

    Create Twitter Bot - Project.py

---
+++ Twitter Bot - Project.py
@@ -0,0 +1,41 @@
+import tweepy
+import time
+
+consumer_key = 'mQTnu2pXGeNYf8wY1G4K1QWZk'
+consumer_secret = 'h5vmpbrhoxkv1dgjoc8ebhmhzfxhcbml'
+access_token = '9fiaz0uanaa7gmhmtrlg4zfbr2hqi7wh'
+access_token_secret = '9afzaN56NJg8x0K7YJ0Zr7c6b3ZU3c2'
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+user = api.me()
+print (user.name) #prints your name.
+print (user.screen_name)
+print (user.followers_count)
+
+search = "zerotomastery"
+numberOfTweets = 2
+
+def limit_handle(cursor):
+  while True:
+    try:
+      yield cursor.next()
+    except tweepy.RateLimitError:
+      time.sleep(1000)
+
+#Be nice to your followers. Follow everyone!
+for follower in limit_handle(tweepy.Cursor(api.followers).items()):
+  if follower.name == 'Usernamehere':
+    print(follower.name)
+    follower.follow()
+
+
+# Be a narcisist and love your own tweets. or retweet anything with a keyword!
+for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print('Retweeted the tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
